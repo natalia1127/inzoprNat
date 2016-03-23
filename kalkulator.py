6ed037a1f28c00c7ac44285bab7cc818d33369a9
Natalia Szymczak <35741@student.ue.poznan.pl>
2016-03-23 12:57:32 +0100

    dodałam funkcje get info

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,11 @@
 def dodawanie():
-a = int(input("Podaj a"))
-b = int(input("Podaj b"))
-print(a+b)
+	a = int(input("Podaj a"))
+	b = int(input("Podaj b"))
+	print(a+b)
 
+def get_info():
+	return "To jest program kalkulator stworzony przez Natalie"
+
+print(get_info())
 dodawanie()
+input()
