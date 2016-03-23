2508cd9c337752a2767e414949908273fd4396a2
Natalia Szymczak <35741@student.ue.poznan.pl>
2016-03-23 13:09:21 +0100

    wprowadzono poprawki do funkcji dodawania

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,9 @@
 def dodawanie(a, b):
-return a+b
-
-a = int(input('podaj pierwsza liczbe'))
-b = int(input('podaj druga liczbe'))
-print(dodawanie(a,b))
+	return a+b
+try:
+	a = int(input('podaj pierwsza liczbe'))	
+	b = int(input('podaj druga liczbe'))
+	print(dodawanie(a,b))
+except ValueError as ve:
+	print("WPROWADZONO BLEDNE DANE, KONCZE DZIALANIE...")
 input()
